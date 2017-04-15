731e6119efcad91c8faad071fabb152326361898
Angela <ang_1416@yahoo.in>
2017-04-15 15:29:41 +0530

    added nav bar

---
+++ library/src/main/java/com/fr0ddy/readless/RssReader.java
@@ -0,0 +1,25 @@
+package com.fr0ddy.readless;
+
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+public class RssReader {
+    private String rssUrl;
+
+    public RssReader(String url) {
+        rssUrl = url;
+    }
+
+    public List<RssItem> getItems() throws Exception {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser saxParser = factory.newSAXParser();
+        //Creates a new RssHandler which will do all the parsing.
+        RssHandler handler = new RssHandler();
+        //Pass SaxParser the RssHandler that was created.
+        saxParser.parse(rssUrl, handler);
+        return handler.getRssItemList();
+    }
+}
